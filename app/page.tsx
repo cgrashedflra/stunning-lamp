b68291ec9d8ef932d99bcc96c1e5982768c7b6e3
Ashfaq <bdrasedas@gmail.com>
2026-04-23 22:33:15 +0600

    working google log in

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { auth } from '@/auth'
+import SignIn from '@/components/sign-in'
+import SignOut from '@/components/sign-out';
+import Email from 'next-auth/providers/email';
 
-const Home = () => {
+ 
+
+const Home = async () => {
+const session = await auth()
+  session ? console.log(`Logged in as ${session.user?.email} ${session.user?.name}`) : console.log("Not logged in");
   return (
-    <div>
-      
+    <div className='flex justify-center items-center min-h-screen'>
+      <SignIn />
+      <SignOut />
     </div>
   )
 }
 
 export default Home
+
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -4,5 +4,5 @@
 import { signIn } from "next-auth/react"
  
 export default function SignIn() {
-  return <button onClick={() => signIn("google")}></button>
+  return <button onClick={() => signIn("google")} className="btn-google">signIn with <span>google</span></button>
 }
